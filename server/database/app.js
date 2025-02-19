f883e8a12235060b592e793814f10cbc0c96c440
ivanismaeel <ivan.ismaeel@gmail.com>
2025-02-19 13:02:29 +0100

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -8,20 +8,20 @@ const port = 3030;
 app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const reviews_data = JSON.parse(fs.readFileSync("data/reviews.json", 'utf8'));
-const dealerships_data = JSON.parse(fs.readFileSync("data/dealerships.json", 'utf8'));
+const reviews_data = JSON.parse(fs.readFileSync('data/reviews.json', 'utf8'));
+const dealerships_data = JSON.parse(fs.readFileSync('data/dealerships.json', 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/", { dbName: 'dealershipsDB' });
+mongoose.connect('mongodb://mongo_db:27017/', { dbName: 'dealershipsDB' });
 
 const Reviews = require('./review');
 const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(() => {
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(() => {
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
 } catch (error) {
   console.error('Error initializing database:', error);
@@ -29,7 +29,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-  res.send("Welcome to the Mongoose API");
+  res.send('Welcome to the Mongoose API');
 });
 
 // Express route to fetch all reviews
@@ -82,22 +82,22 @@ app.get('/fetchDealer/:id', async (req, res) => {
   }
 });
 
-//Express route to insert review
+// Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   const data = JSON.parse(req.body);
   const documents = await Reviews.find().sort({ id: -1 });
-  let new_id = documents[0]['id'] + 1;
+  let new_id = documents[0].id + 1;
 
   const review = new Reviews({
     id: new_id,
-    name: data['name'],
-    dealership: data['dealership'],
-    review: data['review'],
-    purchase: data['purchase'],
-    purchase_date: data['purchase_date'],
-    car_make: data['car_make'],
-    car_model: data['car_model'],
-    car_year: data['car_year'],
+    name: data.name,
+    dealership: data.dealership,
+    review: data.review,
+    purchase: data.purchase,
+    purchase_date: data.purchase_date,
+    car_make: data.car_make,
+    car_model: data.car_model,
+    car_year: data.car_year,
   });
 
   try {
