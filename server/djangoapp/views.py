df7482dcbef1aef0e1b9b2f128039e6c3df61bc4
ivanismaeel <ivan.ismaeel@gmail.com>
2025-02-19 14:12:08 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -120,6 +120,11 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             logger.error(f"Error in posting review: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse
+            (
+                {
+                    "status": 401, "message": "Error in posting review"
+                }
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
