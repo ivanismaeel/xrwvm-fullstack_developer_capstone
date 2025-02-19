7688421b4ea32f57d96ba46d34f112af5b57d2bd
ivanismaeel <ivan.ismaeel@gmail.com>
2025-02-19 14:07:01 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,7 +11,7 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -28,4 +28,4 @@ class CarModel(models.Model):
     )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name}"  # Return the car make and model as the string representation
+        return f"{self.car_make.name} {self.name}"
